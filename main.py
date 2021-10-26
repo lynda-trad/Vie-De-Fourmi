47b8e737c597c197fff5c614ada04bb9b0f61926
lynda-trad <lynda.trad@laplateforme.io>
2021-10-26 11:50:29 +0200

    work on networkx printing; window closes and opens on different position instead of printing in same window again

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import time
 from os.path import exists
 
 import networkx as nx
+import tkinter as tk
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import Anthill
 import fileParsing
@@ -46,8 +49,7 @@ def travel(graph, nodePos, matrix, stepId, anthil):
 
             if int(currentLocation) != 0:  # if ant is not in Sd
                 step = moveToNextRoom(int(currentLocation), currentAnt, anthil, matrix, step)
-            printGraph(graph, nodePos, anthil)
-
+        printGraph(graph, nodePos, anthil)
         stepId += 1
 
     return step
@@ -72,11 +74,19 @@ def initPrintingGraph(anthill):
 
 # Printing NetworkX Graph
 def printGraph(graph, nodePos, anthil):
-    # figure = plt.figure(0)
+    """
+    figure = plt.figure(0)
+    win = tk.Tk()
+    win.geometry('1000x1000')
+    win.title('Anthill')
+    canvas = FigureCanvasTkAgg(figure, win)
+    canvas.set_window_title('Anthill')
+    """
     figure = plt.gcf()
+    # figure = plt.figure(frameon=True)
     figure.canvas.manager.set_window_title('Anthill')
-    # figure.canvas.manager.window.move(0, 0)
     # figure.canvas.manager.window.SetPosition((500, 500))
+    # figure.canvas.manager.window.move(0, 0)
     # figure.canvas.manager.window.setGeometry(50, 100, 640, 545)
 
     nx.draw(graph, nodePos, with_labels=True, font_size=8, alpha=0.8, node_color="#A86CF3")
@@ -87,11 +97,20 @@ def printGraph(graph, nodePos, anthil):
         x, y = pos[currentName]
         plt.text(x, y + 0.1, s=currentCapacity, bbox=dict(facecolor='red', alpha=0.5), horizontalalignment='center')
 
-    plt.savefig("anthill.png")
+    # plt.savefig("anthill.png")
     plt.draw()
-    plt.pause(2)
-    # plt.ion()
-    plt.clf()
+
+    """
+    # canvas.draw()
+    # canvas.get_tk_widget().pack()
+    """
+
+    plt.pause(1)
+    figure.clear()
+    plt.clf()  # clear figure from the canvas
+    """
+    tk.mainloop()
+    """
     plt.close('all')
 
 
